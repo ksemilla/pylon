59f8a274b38e1a958c205e3a1165ce91c738630b
ksemilla <kevinsemilla@gmail.com>
2024-12-09 14:17:41 +1100

    Add role field in user

--- users/models.py
+++ users/models.py
@@ -1,7 +1,13 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    class Roles(models.TextChoices):
+        SUPERUSER = "superuser", _("Super User")
+        ADMIN = "admin", _("Admin")
+        USER = "user", _("User")
+
     firebase_uid = models.CharField(max_length=256)
+    role = models.CharField(max_length=64, choices=Roles, default=Roles.USER)
