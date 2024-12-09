a9304a2ead1b84c416fd600e11b73361ec69d3e6
ksemilla <kevinsemilla@gmail.com>
2024-12-09 13:52:40 +1100

    Add Entity model and migration

--- entities/models.py
+++ entities/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from slugify import slugify
 
 from core.models import StampedModel
 
@@ -6,3 +7,8 @@ from core.models import StampedModel
 class Entity(StampedModel):
     name = models.CharField(max_length=128)
     slug = models.CharField(max_length=256, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.slug = slugify(kwargs["name"])
+        super().save(*args, **kwargs)
